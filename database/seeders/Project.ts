e0b775423ee238642f6490cf155b11da23eeb656
PeacefulDog <nipas.devs@gmail.com>
2023-01-10 15:28:41 +0700

    fix(database): fix seeder not gen same user id for 2 projects

--- database/seeders/Project.ts
+++ database/seeders/Project.ts
@@ -3,12 +3,13 @@ import Project from 'App/Models/Project'
 
 const genUserId = ():string => (Math.floor(Math.random() * 999999999)).toString()
 const genString =  ( ):string => Math.random().toString(36).slice(2, 50);
+const sameUserId = genUserId()
 
 export default class extends BaseSeeder {
   public async run () {
     await Project.createMany([
       {
-        user_id: genUserId(),
+        user_id: sameUserId,
         name: "First Project",
         description: {
           text: genString()
@@ -23,6 +24,20 @@ export default class extends BaseSeeder {
       },
       {
         user_id: genUserId(),
+        name: "Second Project",
+        description: {
+          text: genString()
+        },
+        catagories: [1, 2, 3],
+        funding_goal: 50000,
+        start_date: new Date("2022-03-25"),
+        end_date: new Date("2022-03-25"),
+        status: "Funding",
+        total: 0,
+        backers: 0,
+      },
+      {
+        user_id: sameUserId,
         name: "Second Project",
         description: {
           text: genString()
